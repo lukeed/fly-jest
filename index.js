3d4e53844f02e5b933cbb1e6c11f81dedc2b7fae
Luke Edwards <luke.edwards05@gmail.com>
2017-04-17 18:35:32 -0700

    add basic tests

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const runCLI = require('jest-cli').runCLI;
 module.exports = function (fly) {
 	const rootDir = fly.root;
 	fly.plugin('jest', {every: false}, function * (_, opts) {
-		const config = Object.assign({ rootDir }, opts);
-		runCLI({ config }, rootDir);
+		const config = Object.assign({rootDir}, opts);
+		runCLI({config}, rootDir);
 	});
 };
--- test/index.js
+++ test/index.js
@@ -1,20 +1,16 @@
-const join = require('path').join
-const test = require('tape')
-const Fly = require('fly')
-
-const dir = join(__dirname, 'fixtures')
-const plugins = [require('fly-clear'), require('../')]
-
-const tmpDir = str => join(__dirname, str)
-const create = tasks => new Fly({tasks, plugins})
+const test = require('tape');
+const Fly = require('fly');
 
 test('fly-jest', t => {
-  t.plan(2)
-  const fly = create({
-    *foo(f) {
-      t.true('jest' in f, 'attach `jest` to Task instance')
-      t.true('jest' in fly.plugins, 'attach `jest` plugin to instance')
-    }
-  })
-  fly.start('foo')
-})
+	t.plan(2);
+	const fly = new Fly({
+		plugins: [require('../')],
+		tasks: {
+			*foo(f) {
+				t.true('jest' in f, 'attach `jest` to Task instance');
+				t.true('jest' in fly.plugins, 'attach `jest` plugin to instance');
+			}
+		}
+	});
+	fly.start('foo');
+});
