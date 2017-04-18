2c0ec0f3206e28e3a2312d582d4c7deb519e49e0
Luke Edwards <luke.edwards05@gmail.com>
2017-04-17 18:32:08 -0700

    add module logic

---
+++ index.js
@@ -0,0 +1,9 @@
+const runCLI = require('jest-cli').runCLI;
+
+module.exports = function (fly) {
+	const rootDir = fly.root;
+	fly.plugin('jest', {every: false}, function * (_, opts) {
+		const config = Object.assign({ rootDir }, opts);
+		runCLI({ config }, rootDir);
+	});
+};
